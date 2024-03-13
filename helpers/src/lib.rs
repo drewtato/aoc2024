4d560c810943250f85ebb2a410e8d400307ca0e7
drewtato <15526875+drewtato@users.noreply.github.com>
2024-03-13 16:20:17 -0700

    rand_ext module

--- helpers/src/lib.rs
+++ helpers/src/lib.rs
@@ -1,5 +1,4 @@
-#![feature(array_try_from_fn)]
-#![feature(iter_from_coroutine)]
+#![feature(array_try_from_fn, iter_from_coroutine, type_alias_impl_trait)]
 
 use std::io::stdin;
 use std::ops::{Add, Div, Mul};
@@ -26,40 +25,13 @@ pub use num_integer::*;
 
 pub use primal::*;
 
-pub trait UnwrapDisplay {
-    type Output;
-    /// Like [`Result::unwrap`], but prints with `Display` instead of `Debug`.
-    ///
-    /// # Examples
-    ///
-    /// Unwrapping an `Ok` works normally.
-    ///
-    /// ```
-    /// # use helpers::UnwrapDisplay;
-    /// let opt: Result<i32, &'static str> = Ok(4);
-    /// assert_eq!(4, opt.unwrap_display());
-    /// ```
-    ///
-    /// Unwrapping an `Err` prints using `Display` and panics.
-    ///
-    /// ```should_panic
-    /// # use helpers::UnwrapDisplay;
-    /// let opt: Result<i32, &'static str> = Err("oh no");
-    /// opt.unwrap_display(); // panics
-    /// ```
-    fn unwrap_display(self) -> Self::Output;
-}
+pub use rand::{self, Rng};
 
-impl<T, E> UnwrapDisplay for Result<T, E>
-where
-    E: Display,
-{
-    type Output = T;
+mod unwrap_display;
+pub use unwrap_display::*;
 
-    fn unwrap_display(self) -> Self::Output {
-        self.unwrap_or_else(|e| panic!("{e}"))
-    }
-}
+mod rand_ext;
+pub use rand_ext::*;
 
 /// Short version of [`Default::default`].
 pub fn def<D: Default>() -> D {
---
+++ helpers/src/rand_ext.rs
@@ -0,0 +1,44 @@
+use std::ops::DerefMut;
+use std::sync::{Mutex, OnceLock};
+
+use rand::distributions::{DistIter, Distribution, Standard};
+use rand::{Rng, SeedableRng};
+use rand_xorshift::XorShiftRng;
+
+static GLOBAL_RNG: OnceLock<Mutex<DefaultRng>> = OnceLock::new();
+pub type DefaultRng = impl Rng;
+
+pub fn global_rng() -> impl DerefMut<Target = DefaultRng> {
+    GLOBAL_RNG.get_or_init(|| Mutex::new(rng())).lock().unwrap()
+}
+
+pub fn random<T>() -> T
+where
+    Standard: Distribution<T>,
+{
+    global_rng().gen()
+}
+
+pub fn rng() -> DefaultRng {
+    XorShiftRng::seed_from_u64(2024)
+}
+
+pub trait RngExt: Rng {
+    /// Borrows `self`.
+    ///
+    /// This exists for the same reason [`Iterator::by_ref`] exists.
+    fn by_ref(&mut self) -> &mut Self {
+        self
+    }
+
+    /// Calls [`Rng::sample_iter`] with the [`Standard`] distribution.
+    ///
+    /// This function is to `sample_iter` as [`Rng::gen`] is to [`Rng::sample`].
+    fn gen_iter<T>(self) -> DistIter<Standard, Self, T>
+    where
+        Standard: Distribution<T>,
+        Self: Sized,
+    {
+        self.sample_iter(Standard)
+    }
+}
