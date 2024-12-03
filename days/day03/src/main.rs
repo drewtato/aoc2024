03c5c8570ff96a48d6ee5a661fe997294f601bde
Drewtato <15526875+drewtato@users.noreply.github.com>
2024-12-02 21:21:20 -0800

    day 3 done

--- days/day03/src/main.rs
+++ days/day03/src/main.rs
@@ -1,15 +1,3 @@
-#![feature(type_alias_impl_trait)]
-#![feature(array_windows)]
-#![feature(array_chunks)]
-#![feature(slice_take)]
-#![feature(iter_array_chunks)]
-#![feature(split_as_slice)]
-#![feature(coroutines)]
-#![feature(slice_split_once)]
-#![feature(impl_trait_in_assoc_type)]
-#![feature(array_try_map)]
-#![allow(unused)]
-
 use helpers::*;
 
 fn main() {
@@ -21,10 +9,31 @@ struct Solver;
 
 impl solver_interface::ChildSolver for Solver {
     fn part_one(input: &[u8], _debug: u8) -> impl Display + 'static {
-        "unimplemented"
+        let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+        let mut sum = 0;
+        for m in re.captures_iter(input) {
+            let n1: i32 = parse_ascii(&m[1]).unwrap();
+            let n2: i32 = parse_ascii(&m[2]).unwrap();
+            sum += n1 * n2;
+        }
+        sum
     }
 
     fn part_two(input: &[u8], _debug: u8) -> impl Display + 'static {
-        "unimplemented"
+        let re = Regex::new(r"(mul\((\d+),(\d+)\))|(do\(\))|(don't\(\))").unwrap();
+        let mut sum = 0;
+        let mut enabled = true;
+        for m in re.captures_iter(input) {
+            if m.get(1).is_some() && enabled {
+                let n1: i32 = parse_ascii(&m[2]).unwrap();
+                let n2: i32 = parse_ascii(&m[3]).unwrap();
+                sum += n1 * n2;
+            } else if m.get(4).is_some() {
+                enabled = true;
+            } else if m.get(5).is_some() {
+                enabled = false;
+            }
+        }
+        sum
     }
 }
