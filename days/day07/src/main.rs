6412c0b885ef4935637b329240ca5a7d601948c5
Drewtato <15526875+drewtato@users.noreply.github.com>
2024-12-07 22:16:48 -0800

    day 7 done

--- days/day07/src/main.rs
+++ days/day07/src/main.rs
@@ -1,15 +1,3 @@
-#![feature(type_alias_impl_trait)]
-#![feature(array_windows)]
-#![feature(array_chunks)]
-#![feature(slice_take)]
-#![feature(iter_array_chunks)]
-#![feature(split_as_slice)]
-#![feature(coroutines)]
-#![feature(slice_split_once)]
-#![feature(impl_trait_in_assoc_type)]
-#![feature(array_try_map)]
-#![allow(unused)]
-
 use helpers::*;
 
 fn main() {
@@ -21,10 +9,81 @@ struct Solver;
 
 impl solver_interface::ChildSolver for Solver {
     fn part_one(input: &[u8], _debug: u8) -> impl Display + 'static {
-        "unimplemented"
+        let mut con = Consume::new(input);
+        let mut sum = 0;
+        let mut possibles: Vec<u64> = Vec::new();
+        let mut possibles_temp = Vec::new();
+        while !con.is_empty() {
+            let total = con.int().unwrap();
+            con.byte(b':');
+            possibles.push(0);
+
+            while !con.newline() {
+                con.byte(b' ');
+                let n: u64 = con.int().unwrap();
+                for &p in &possibles {
+                    for new in [p * n, p + n] {
+                        if new <= total {
+                            possibles_temp.push(new);
+                        }
+                    }
+                }
+                swap(&mut possibles_temp, &mut possibles);
+                possibles_temp.clear();
+            }
+
+            if possibles.contains(&total) {
+                sum += total;
+            }
+
+            possibles.clear();
+        }
+        sum
     }
 
     fn part_two(input: &[u8], _debug: u8) -> impl Display + 'static {
-        "unimplemented"
+        let mut con = Consume::new(input);
+        let mut sum = 0;
+        let mut possibles: Vec<u64> = Vec::new();
+        let mut possibles_temp = Vec::new();
+        while !con.is_empty() {
+            let total = con.int().unwrap();
+            con.byte(b':');
+            possibles.push(0);
+
+            while !con.newline() {
+                con.byte(b' ');
+                let n: u64 = con.int().unwrap();
+                for &p in &possibles {
+                    // eprintln!("{p} || {n} = {:?}", concatenate(p, n));
+                    for new in [p.checked_mul(n), p.checked_add(n), concatenate(p, n)]
+                        .into_iter()
+                        .flatten()
+                    {
+                        if new <= total {
+                            possibles_temp.push(new);
+                        }
+                    }
+                }
+                swap(&mut possibles_temp, &mut possibles);
+                possibles_temp.clear();
+            }
+
+            // eprintln!("{total}: {possibles:?}");
+            if possibles.contains(&total) {
+                sum += total;
+            }
+
+            possibles.clear();
+        }
+        sum
     }
+}
+
+fn concatenate(p: u64, n: u64) -> Option<u64> {
+    if p == 0 {
+        return None;
+    }
+    let len = n.checked_ilog10()?;
+    p.checked_mul(10_u64.checked_pow(len + 1)?)?.checked_add(n)
 }
